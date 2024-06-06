56b32605bd06c2111eda0a97b884cf72a9d94d32
Centralice <108938330+Centralice@users.noreply.github.com>
2024-06-07 01:19:50 +0200

    done

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,50 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const input = document.querySelector('input[name="delay"]');
+const submit = document.querySelector('form');
+
+input.addEventListener('input', handleInput);
+submit.addEventListener('submit', handleSubmit);
+
+
+let delay;
+
+function handleInput(event) {
+  delay = event.target.value;
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const fulfilled = document.querySelector('input[value="fulfilled"]:checked');
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (fulfilled) {
+        resolve(`Fulfilled promise in ${delay}ms`);                               
+      } else {
+        reject(`Rejected promise in ${delay}ms`);
+      }
+
+      promise.then(value => {
+        iziToast.success({
+          message: value,
+          messageColor: '#ffffff',
+          backgroundColor: '#008000',
+          position: 'bottomRight',
+          timeout: 7000,
+        });}
+      )
+      .catch(error => {
+        iziToast.error({
+          message: error,
+          messageColor: '#ffffff',
+          backgroundColor: '#d00000',
+          position: 'topRight',
+          timeout: 7000,
+        });;
+      });
+    }, delay);
+  });
+}
+
 
